4cdb76df6fc8834484b4084d2e2d9ad271db126a
Oskar Albers <o.albers@blokkerholding.nl>
2017-09-01 08:09:40 +0200

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Import the DataService
+import { DataService } from './data.service';
+import { HackerNewsService} from './app.service';
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+
+
+
+export class HackerNewsComponent{
+  news = {};
+  constructor(private HackerNewsService:HackerNewsService){
+    this.HackerNewsService.getNews().subscribe(data => this.news = data);
+  }
+}
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesAsync().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { DataService } from './data.service';
 import { AppComponent } from './app.component';
+import { HackerNewsComponent } from './app.component';
+import { HackerNewsService } from './app.service';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [DataService, HackerNewsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
